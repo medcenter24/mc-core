e25f662bea6946e4044cb8727a078e8b42b92019
zagovorichev <zagovorichev@gmail.com>
2017-05-02 19:51:48 +0300

    added interfaces for dom parsing

---
+++ app/Services/Parser/Dom/CaseParserProviderInterface.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Copyright (c) 2017.
+ *
+ * @author Alexander Zagovorichev <zagovorichev@gmail.com>
+ */
+
+/**
+ * Created by PhpStorm.
+ * User: Alexander
+ * Date: 02.05.2017
+ * Time: 19:50
+ */
+
+namespace app\Services\Parser\Dom;
+
+
+interface CaseParserProviderInterface extends DomParserProviderInterface
+{
+    public function getTitle();
+    public function getPatientName();
+    public function getDoctorName();
+    public function getSevices();
+    public function getSurvices();
+}

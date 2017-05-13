8927330406eea75d66f140ef7774573a104bb036
zagovorichev <zagovorichev@gmail.com>
2017-05-13 23:37:08 +0300

    Importer

--- app/Services/Import/Dhv24/Dhv24Docx2017Provider.php
+++ app/Services/Import/Dhv24/Dhv24Docx2017Provider.php
@@ -8,7 +8,10 @@
 namespace App\Services\Import\Dhv24;
 
 
+use app\Services\DocxReader\DocxReaderInterface;
 use App\Services\DocxReader\SimpleDocxReaderService;
+use App\Services\DomDocumentService;
+use App\Services\ExtractTableFromArrayService;
 use App\Services\Import\DataProvider;
 
 class Dhv24Docx2017Provider extends DataProvider
@@ -18,10 +21,47 @@ class Dhv24Docx2017Provider extends DataProvider
      */
     private $readerService;
 
+    /**
+     * @var ExtractTableFromArrayService
+     */
+    private $tableExtractorService;
+
+    /**
+     * @var DomDocumentService
+     */
+    private $domService;
+
+    public function __construct(DocxReaderInterface $readerService = null, $tableExtractorService = null, $domService = null)
+    {
+        if ($readerService) {
+            $this->readerService = $readerService;
+        } else {
+            $this->readerService = new SimpleDocxReaderService();
+        }
+
+        if ($tableExtractorService) {
+            $this->tableExtractorService = $tableExtractorService;
+        } else {
+            $this->tableExtractorService = new ExtractTableFromArrayService([
+                ExtractTableFromArrayService::CONFIG_TABLE => ['w:tbl'],
+                ExtractTableFromArrayService::CONFIG_ROW => ['w:tr'],
+                ExtractTableFromArrayService::CONFIG_CEIL => ['w:tc'],
+            ]);
+        }
+
+        if ($domService) {
+            $this->domService = $domService;
+        } else {
+            $this->domService = new DomDocumentService([
+                DomDocumentService::STRIP_STRING => true,
+                DomDocumentService::CONFIG_WITHOUT_ATTRIBUTES => true,
+            ]);
+        }
+    }
+
     public function load($path = '')
     {
-        $readerService = new SimpleDocxReaderService();
-        $readerService->load($path);
+        $this->readerService->load($path);
         return $this;
     }
 
@@ -36,6 +76,19 @@ class Dhv24Docx2017Provider extends DataProvider
             'Дата,  место, время визита',
             'Fecha, lugar de visita SPAIN'
         ];
-        mb_strpos($this->readerService->getText(), );
+
+        foreach ($points as $checkPoint) {
+            if (mb_strpos($this->readerService->getText(), $checkPoint) !== false) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public function import()
+    {
+        $data = $this->tableExtractorService->extract($this->domService->toArray($this->readerService->getDom()));
+        $tables = $data[ExtractTableFromArrayService::TABLES];
     }
 }

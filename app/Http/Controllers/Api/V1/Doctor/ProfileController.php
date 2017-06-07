2b36fa4cb4d475752f3e8555aff9c6a97d0a96e0
MedCenter24 Dev <dev@medcenter24.com>
2017-06-07 22:21:05 +0300

    added profile controller

---
+++ app/Http/Controllers/Api/V1/Doctor/ProfileController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1\Doctor;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class ProfileController extends Controller
+{
+    //
+}

42786b6856f7d7dff027d254246b10037226bcbd
MedCenter24 Dev <dev@medcenter24.com>
2017-06-11 00:42:53 +0300

    implementation of the doctor gui

--- app/Http/Controllers/Api/V1/Doctor/AccidentsController.php
+++ app/Http/Controllers/Api/V1/Doctor/AccidentsController.php
@@ -7,7 +7,16 @@
 
 namespace App\Http\Controllers\Api\V1\Doctor;
 
+use App\Accident;
+use App\DoctorAccident;
 use App\Http\Controllers\ApiController;
+use App\Transformers\AccidentTransformer;
+use App\Transformers\AccidentTypeTransformer;
+use App\Transformers\CaseAccidentTransformer;
+use App\Transformers\DiagnosticTransformer;
+use App\Transformers\DoctorAccidentStatusTransformer;
+use App\Transformers\DoctorServiceTransformer;
+use App\Transformers\PatientTransformer;
 use Illuminate\Http\Request;
 
 class AccidentsController extends ApiController
@@ -17,45 +26,91 @@ class AccidentsController extends ApiController
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        $rows = $request->get('per_page', 10);
+        $accidents = Accident::orderBy('created_at', 'desc')
+            ->paginate($rows, $columns = ['*'], $pageName = 'page', $request->get('page', null));
+        return $this->response->paginator($accidents, new CaseAccidentTransformer());
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
+     * Closed or accident which were sent which can't be changed
      *
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
     public function show($id)
     {
-        //
+        $accident = Accident::find($id);
+        if (!$accident) {
+            $this->response->errorNotFound();
+        }
+
+        return $this->response->item($accident, new AccidentTransformer());
+    }
+
+    public function patient($id)
+    {
+        $accident = Accident::find($id);
+        if (!$accident) {
+            $this->response->errorNotFound();
+        }
+        $patient = $accident->patient;
+        if (!$patient) {
+            $this->response->errorNotFound();
+        }
+
+        return $this->response->item($patient, new PatientTransformer());
+    }
+
+    public function status($id)
+    {
+        $accident = Accident::find($id);
+        if (!$accident) {
+            $this->response->errorNotFound();
+        }
+
+        $doctorAccident = $accident->caseable;
+        if (!$doctorAccident || !is_a($doctorAccident, DoctorAccident::class)) {
+            $this->response->errorNotFound();
+        }
+
+        return $this->response->item($doctorAccident, new DoctorAccidentStatusTransformer());
+    }
+
+    public function services($id)
+    {
+        $accident = Accident::find($id);
+        if (!$accident) {
+            $this->response->errorNotFound();
+        }
+
+        return $this->response->collection($accident->services, new DoctorServiceTransformer());
+    }
+
+    public function type($id)
+    {
+        $accident = Accident::find($id);
+        if (!$accident) {
+            $this->response->errorNotFound();
+        }
+
+        return $this->response->item($accident->type, new AccidentTypeTransformer());
+    }
+
+    public function surveys($id)
+    {
+        $accident = Accident::find($id);
+        if (!$accident) {
+            $this->response->errorNotFound();
+        }
+
+        return $this->response->collection($accident->diagnostics, new DiagnosticTransformer());
     }
 
     /**
-     * Show the form for editing the specified resource.
+     * Edit form for the accident which should be edited by the doctor
      *
      * @param  int  $id
      * @return \Illuminate\Http\Response
@@ -78,7 +133,7 @@ class AccidentsController extends ApiController
     }
 
     /**
-     * Remove the specified resource from storage.
+     * Reject the accident
      *
      * @param  int  $id
      * @return \Illuminate\Http\Response

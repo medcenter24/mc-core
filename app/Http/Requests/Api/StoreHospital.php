abe9fac31e0cf4ebcf4052b9d2e255145d8612d6
Alexander Zagovorichev <zagovorichev@gmail.com>
2017-06-19 18:41:40 +0300

    hospitals and cities

---
+++ app/Http/Requests/Api/StoreHospital.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Copyright (c) 2017.
+ *
+ * @author Alexander Zagovorichev <zagovorichev@gmail.com>
+ */
+
+namespace App\Http\Requests\Api;
+
+class StoreHospital extends JsonRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'title' => 'required|max:150',
+            'description' => 'max:255',
+            'ref_key' => 'required|max:5|unique:doctors',
+            'address' => 'max:255',
+            'phones' => 'max:200',
+        ];
+    }
+}

6bf1fb184e556d35a305454d0ee702f33d77a66e
MedCenter24 Dev <dev@medcenter24.com>
2017-08-15 13:47:50 +0300

    improvement for the income calculation

--- database/migrations/2017_02_05_183737_create_accidents_table.php
+++ database/migrations/2017_02_05_183737_create_accidents_table.php
@@ -30,6 +30,12 @@ class CreateAccidentsTable extends Migration
             $table->unsignedInteger('city_id')->default(0)->index();
             $table->unsignedInteger('discount_id')->default(0)->index();
             $table->decimal('discount_value', 8, 2)->default(0);
+            // payment which should be done to the doctor or hospital
+            $table->decimal('caseable_cost', 8, 2)->default(0);
+            // total income from this case
+            $table->decimal('income', 8, 2)->default(0);
+            // if income is fixed it won't be changed automatically (only hand mode)
+            $table->unsignedTinyInteger('fixed_income')->default(0);
             $table->string('assistant_ref_num')->default('')->index();
             $table->morphs('caseable');
             $table->string('ref_num')->default('')->index();

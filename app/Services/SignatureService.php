ed8e7b9109b00369e01ceebe7e5402dab330497c
MedCenter24 Dev <dev@medcenter24.com>
2017-12-13 09:46:22 +0300

    added company model

---
+++ app/Services/SignatureService.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Copyright (c) 2017.
+ *
+ * @author Alexander Zagovorichev <zagovorichev@gmail.com>
+ */
+
+namespace App\Services;
+
+
+class SignatureService
+{
+    const DISC = 'signature';
+    const FOLDER = 'media';
+}

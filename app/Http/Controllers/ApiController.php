f0d431fa560324415badd1257c349ad174f5d608
MedCenter24 Dev <dev@medcenter24.com>
2017-12-14 19:21:38 +0300

    fixed authorizations also added unit tests for it

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -10,8 +10,15 @@ namespace App\Http\Controllers;
 use Dingo\Api\Routing\Helpers;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 
 class ApiController extends Controller
 {
     use Helpers;
+
+    public function __construct()
+    {
+        parent::__construct();
+        Auth::setDefaultDriver('api');
+    }
 }

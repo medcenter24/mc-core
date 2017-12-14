d0cc4503090ff4dae02823f30285dedd256ab9c5
MedCenter24 Dev <dev@medcenter24.com>
2017-12-14 19:24:31 +0300

    added important commentary

--- config/auth.php
+++ config/auth.php
@@ -19,6 +19,8 @@ return [
     */
 
     'defaults' => [
+        // if you make 'api' guard then all application will work in the API mode
+        // that means that there won't be admin panel
         'guard' => 'web',
         'passwords' => 'users',
     ],

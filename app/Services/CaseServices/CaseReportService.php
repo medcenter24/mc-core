a8501bd8e1e693bd612414db33ff7415c0799399
MedCenter24 Dev <dev@medcenter24.com>
2017-12-31 14:16:50 +0300

    fixed pdf

--- app/Services/CaseServices/CaseReportService.php
+++ app/Services/CaseServices/CaseReportService.php
@@ -37,19 +37,60 @@ class CaseReportService
         return view('case.report', ['report' => $this->report]);
     }
 
+    public function htmlDocuments() {
+        return view('case.documents', ['report' => $this->report]);
+    }
+
     public function toPdf()
     {
-        $mpdf = new Mpdf();
-        $mpdf->WriteHTML($this->toHtml());
-        $mpdf->Output(\Storage::disk(self::PDF_DISK)->path($this->getUniquePdfFileName()), Destination::FILE);
+        try {
+            $mpdf = new Mpdf([
+                'debug' => true,
+                'useSubstitutions' => false,
+                'simpleTables' => true,
+                'use_kwt' => true,
+                'shrink_tables_to_fit' => 1,
+                'showImageErrors' => true,
+                /*'allowCJKorphans' => false,
+                'allowCJKoverflow' => true,*/
+                /*'ignore_table_percents' => true,
+                'ignore_table_widths' => true,*/
+                /*'keepColumns' => true,
+                'keep_table_proportions' => true,*/
+                // 'justifyB4br' => true,
+                'margin_left' => 3,
+                'margin_right' => 5,
+                'margin_top' => 9,
+                'margin_bottom' => 1,
+                'margin_header' => 0,
+                'margin_footer' => 0,
+            ]);
+            $mpdf->WriteHTML($this->toHtml());
+
+            $mpdf->SetTitle('Report ' . $this->report->uniqueIdentifier());
+            $mpdf->SetAuthor("MyDoctors24.com");
+            /*$mpdf->SetWatermarkText("Paid");
+            $mpdf->showWatermarkText = true;
+            $mpdf->watermark_font = 'DejaVuSansCondensed';
+            $mpdf->watermarkTextAlpha = 0.1;*/
+            $mpdf->AddPage();
+
+            $mpdf->WriteHTML($this->documentsHtml());
+
+            $mpdf->SetDisplayMode('fullpage');
+
+            $mpdf->Output(\Storage::disk(self::PDF_DISK)->path($this->getUniquePdfFileName()), Destination::FILE);
+        } catch (\Mpdf\MpdfException $e) {
+            \Log::debug($e->getMessage());
+        }
     }
 
     public function getPdfPath()
     {
-        $fileName = $this->getUniquePdfFileName();
-        if (!\Storage::disk(self::PDF_DISK)->exists($fileName)) {
+        /*$fileName = $this->getUniquePdfFileName();
+        if (!\Storage::disk(self::PDF_DISK)->exists($fileName)) {*/
             $this->toPdf();
-        }
+        // }
 
         return \Storage::disk(self::PDF_DISK)->path($this->getUniquePdfFileName());
     }
--- resources/lang/en/dhv24.php
+++ resources/lang/en/dhv24.php
@@ -8,19 +8,20 @@
 return [
     'report' => [
         'title' => 'MEDICAL REPORT, INVOICE',
-        'company_description' => 'MEDICAL SERVICES NETWORK IN SPAIN, ANDORRA <span style="color: red">24h</span>',
+        'company_description' => '<p style="font-size: 20px; font-family: Verdana;">
+            MEDICAL SERVICES NETWORK IN SPAIN, ANDORRA <b style="color: #cc0033; font-family: Arial">24h</b></p>',
         'company_title' => '
-            <span style="font-size: 30px">
-                D<span style="color: blue">octor</span> H<span style="color: blue">ome</span> V<span style="color: blue">isit</span>
+            <span style="font-size: 34px;">
+                <strong>D<span style="color: #0066ff;">octor</span> H<span style="color: #0066ff">ome</span> V<span style="color: #0066ff">isit</span></strong>
             </span>
-            <b><i>S.L.P., NIF: B55570451</i></b>',
+            <strong style="color: #333;font-size: 12px;"><i>S.L.P., NIF: B55570451</i></strong>',
         'company_contacts' => 'Medical centre address: carrer Major 24, Salou 43840, España<br>
             Fiscal/postal address: carrer Deessa Isis 7-4, Miami Playa 43892<br>
             tel. +34.977.172.169 (24h), www.dhv24.com, dhvisit@gmail.com
             ',
         'company_info' => 'El contenido de esta transmision  de fax es confidencial y esta sujeta a las
             disposiciones legales aplicables. Si usted no es el destinatario no puede hacer uso,
-            distribuir o copiar esta informacion. Disculpe si ha recibido esta transmision por error.
+            distribuir o copiar esta informacion. Disculpe si ha recibido esta transmision por error.<br>
             Por favor notifiquenoslo inmediatamente y devuelva el original por correo',
         'floating_line' => 'DOCTOR HOME VISIT S.L. Ha sido inscrita con fecha 18.04.2012 en el 
             Registro Mercantil de Tarragona, Tomo 2678, Folio 122 
@@ -40,7 +41,9 @@ return [
         'service_description' => 'Import, €',
         'service_footer_description' => 'Exento de IVA /Art.20 uno /Ley 37/1992',
         'service_footer_title' => 'TOTAL IMPORT, EUR',
-        'visit_info_title' => 'Дата, место, время визита<br>Fecha, lugar de visita [SPAIN]',
+        'visit_info_title' => 'Дата, место, время визита',
+        'visit_info_place' => 'Fecha, lugar de visita',
+        'visit_country' => 'SPAIN',
         'bank_title' => 'DEUTSCHE BANK,',
         'bank_address' => 'Rambla Nova, 115, 43001 Tarragona, Spain',
         'bank_details_label' => 'Details of Bank Account:',
@@ -50,5 +53,6 @@ return [
         'bank_iban' => 'ES98 0019 0080 2840 1004 0674',
         'bank_swift_label' => 'SWIFT:',
         'bank_swift' => 'DEUTESBB',
+        'currency' => '€',
     ]
 ];

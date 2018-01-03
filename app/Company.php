dbda21b9e9d3192a36b630320d7c98f12fd5f47a
MedCenter24 Dev <dev@medcenter24.com>
2018-01-03 19:11:51 +0300

    fixed medias - avoid usage of the public directory

--- app/Company.php
+++ app/Company.php
@@ -23,18 +23,25 @@ class Company extends Model implements HasMedia, HasMediaConversions
     use SoftDeletes;
     use HasMediaTrait;
 
+    const THUMB_250 = 'thumb_250';
+    const THUMB_300X100 = 'thumb_300x100';
+
     protected $fillable = ['title'];
     protected $visible = ['title'];
 
+    /**
+     * @param Media|null $media
+     * @throws \Spatie\Image\Exceptions\InvalidManipulation
+     */
     public function registerMediaConversions(Media $media = null)
     {
-        $this->addMediaConversion('thumb_250')
+        $this->addMediaConversion(self::THUMB_250)
             ->sharpen(10)
             ->quality(80)
             ->fit(Manipulations::FIT_CROP, 250, 250)
             ->performOnCollections(LogoService::FOLDER);
 
-        $this->addMediaConversion('thumb_300x100')
+        $this->addMediaConversion(self::THUMB_300X100)
             ->sharpen(10)
             ->quality(80)
             ->fit(Manipulations::FIT_CROP, 300, 100)

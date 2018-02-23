119c34cced1b38ead8811179fc0a42526d81eae3
MedCenter24 Dev <dev@medcenter24.com>
2018-02-23 14:02:54 +0300

    Updated Laravel to version 5.6

--- tests/CreatesApplication.php
+++ tests/CreatesApplication.php
@@ -8,6 +8,7 @@
 namespace Tests;
 
 use Illuminate\Contracts\Console\Kernel;
+use Illuminate\Support\Facades\Hash;
 
 trait CreatesApplication
 {
@@ -23,6 +24,8 @@ trait CreatesApplication
         $app = require __DIR__.'/../bootstrap/app.php';
 
         $app->make(Kernel::class)->bootstrap();
+
+        Hash::driver('bcrypt')->setRounds(4);
 
         return $app;
     }

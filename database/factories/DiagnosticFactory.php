658bd5d598f760305478d1afda0c0c1d65f9603c
MedCenter24 Dev <dev@medcenter24.com>
2018-06-12 00:46:34 +0300

    fixed seeds added support of the accidents search for api

--- database/factories/DiagnosticFactory.php
+++ database/factories/DiagnosticFactory.php
@@ -9,9 +9,10 @@ use App\DiagnosticCategory;
 use Faker\Generator as Faker;
 
 $factory->define(App\Diagnostic::class, function (Faker $faker) {
+    $refKey = $faker->toUpper(str_random(3));
     return [
-        'title' => $faker->text(20),
-        'disease_code' => str_random(3),
+        'title' => 'Diagnostic ' . $refKey,
+        'disease_code' => $refKey,
         'description' => $faker->text(),
         'diagnostic_category_id' => function () {
             return factory(DiagnosticCategory::class)->create()->id;

6b409763fe909ba13c90532cd9c1fa0aa3bd3064
MedCenter24 Dev <dev@medcenter24.com>
2018-09-12 15:27:06 +0300

    restored guarantees + added statuses to invoices

--- database/factories/GuaranteeFactory.php
+++ database/factories/GuaranteeFactory.php
@@ -11,9 +11,6 @@ use Faker\Generator as Faker;
 $factory->define(App\Guarantee::class, function (Faker $faker) {
     return [
         'title' => $faker->text(30),
-        'form_report_id' => function () {
-            return factory(FormReport::class)->create()->id;
-        },
         'status' => 'new',
     ];
 });

d3ba7060b5e3d60564919803d8dcc4360134be32
MedCenter24 Dev <dev@medcenter24.com>
2019-02-04 18:31:46 +0300

    finance calculations for all models

--- database/factories/HospitalAccidentFactory.php
+++ database/factories/HospitalAccidentFactory.php
@@ -12,7 +12,5 @@ $factory->define(App\HospitalAccident::class, function (Faker $faker) {
         'hospital_id' => $faker->numberBetween(1, 10),
         'hospital_guarantee_id' => $faker->numberBetween(1, 10),
         'hospital_invoice_id' => $faker->numberBetween(1, 10),
-        'assistant_invoice_id' => $faker->numberBetween(1, 10),
-        'assistant_guarantee_id' => $faker->numberBetween(1, 10),
     ];
 });

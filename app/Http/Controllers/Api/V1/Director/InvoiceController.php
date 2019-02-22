1b283482540911c80c366056feebd9dbf2e51cac
MedCenter24 Dev <dev@medcenter24.com>
2019-02-22 17:49:36 +0300

    fixed fixedPayment

--- app/Http/Controllers/Api/V1/Director/InvoiceController.php
+++ app/Http/Controllers/Api/V1/Director/InvoiceController.php
@@ -18,6 +18,7 @@ use App\Transformers\FormTransformer;
 use App\Transformers\InvoiceTransformer;
 use App\Transformers\UploadedFileTransformer;
 use App\Upload;
+use Dingo\Api\Http\Response;
 use League\Fractal\TransformerAbstract;
 
 class InvoiceController extends ApiController
@@ -36,7 +37,12 @@ class InvoiceController extends ApiController
         return Invoice::class;
     }
 
-    public function update($id, InvoiceRequest $request)
+    /**
+     * @param $id
+     * @param InvoiceRequest $request
+     * @return \Dingo\Api\Http\Response
+     */
+    public function update($id, InvoiceRequest $request): Response
     {
         $invoice = Invoice::findOrFail($id);
         if (!$invoice) {
@@ -57,7 +63,7 @@ class InvoiceController extends ApiController
         return $this->response->accepted(null, $transformer->transform($invoice));
     }
 
-    private function assignInvoiceTypeResource(Invoice $invoice, $request)
+    private function assignInvoiceTypeResource(Invoice $invoice, $request): void
     {
         if ($invoice->type === 'form' && $request->json('formId', false)) {
             $form = Form::findOrFail($request->json('formId'));
@@ -74,7 +80,7 @@ class InvoiceController extends ApiController
         }
     }
 
-    public function store(InvoiceRequest $request)
+    public function store(InvoiceRequest $request): Response
     {
         $invoice = Invoice::create([
             'title' => $request->json('title', ''),
@@ -92,11 +98,12 @@ class InvoiceController extends ApiController
         return $this->response->created(null, $transformer->transform($invoice));
     }
 
-    private function assignPayment(Invoice &$invoice, $price = 0, FinanceCurrency $currency = null)
+    private function assignPayment(Invoice $invoice, $price = 0, FinanceCurrency $currency = null): void
     {
         $payment = Payment::create([
             'value' => $price,
             'currency_id' => 0,
+            'fixed' => 0,
         ]);
         $invoice->payment = $payment;
         $invoice->save();
@@ -107,10 +114,10 @@ class InvoiceController extends ApiController
      * @param $id
      * @return \Dingo\Api\Http\Response
      */
-    public function form($id)
+    public function form($id): Response
     {
         /** @var Invoice $invoice */
-        $invoice = Invoice::find($id);
+        $invoice = Invoice::findOrFail($id);
         $form = $invoice->forms()->first();
         if (!$invoice->forms()->count()) {
             return $this->response->noContent();
@@ -123,10 +130,10 @@ class InvoiceController extends ApiController
      * @param $id
      * @return \Dingo\Api\Http\Response
      */
-    public function file($id)
+    public function file($id): Response
     {
         /** @var Invoice $invoice */
-        $invoice = Invoice::find($id);
+        $invoice = Invoice::findOrFail($id);
         $upload = $invoice->uploads()->first();
         if (!$invoice->uploads()->count()) {
             return $this->response->noContent();

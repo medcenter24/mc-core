67ff015c7ae72d87b2865c4a8df5becd0f64060c
MedCenter24 Development <dev@medcenter24.com>
2019-09-27 11:52:16 +0300

    fixed breadcrumbs

--- resources/views/admin/html/breadcrumbs.blade.php
+++ resources/views/admin/html/breadcrumbs.blade.php
@@ -1,4 +1,25 @@
-@if ( isset ($menuService) && !empty($menuService->asArray()) )
+@php
+function printMenu ($menu) {
+    foreach ( $menu as $item ) {
+        if (array_key_exists('active', $item) && $item['active']) {
+            echo '<li class="breadcrumb-item">';
+            if(isset($item['slug'])) {
+                echo '<a href="'.url($item['slug']).'">'.$item['name'].'</a>';
+            } else {
+                echo $item['name'];
+            }
+            echo '</li>';
+            $menu = is_array($item) && array_key_exists('submenu', $item) ? $item['submenu'] : false;
+            if ($menu) {
+                printMenu($menu);
+            }
+            break;
+        }
+    }
+}
+@endphp
+
+@if ( isset ($menuService) && count($menuService->asArray()) )
     <nav aria-label="breadcrumb">
         <ol class="breadcrumb">
 
@@ -6,22 +27,7 @@
                 <a href="{{ url('admin') }}" class="breadcrumb-item">@lang('content.main')</a>
             </li>
             @php
-                $menu = $menuService->asArray();
-                while ($menu) {
-                    foreach ( $menu as $item ) {
-                        if (array_key_exists('active', $item) && $item['active']) {
-                            echo '<li class="breadcrumb-item">';
-                            if(isset($item['slug'])) {
-                                echo '<a href="'.url($item['slug']).'">'.$item['name'].'</a>';
-                            } else {
-                                echo $item['name'];
-                            }
-                            echo '</li>';
-                            $menu = is_array($item) && array_key_exists('submenu', $item) ? $item['submenu'] : false;
-                            break;
-                        }
-                    }
-                }
+                printMenu($menuService->asArray());
             @endphp
         </ol>
     </nav>
